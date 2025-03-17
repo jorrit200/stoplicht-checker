9e9eaf0cf45ec1173a6b236dd2d633fa523a80c6
jorrit <jorritdehaan200@gmail.com>
2025-03-17 21:17:23 +0100

    ADD: Live protocol verkeers data

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
 ﻿import {Subscriber} from "zeromq";
 import {TopicCheckerResult, ZMQSubCheckerBinder} from "./Service/ZMQSubCheckerBinder";
 import {LogConclusionAsMarkDown} from "./Loggers/LogConclusionAsMarkDown";
-
-
+import {TrafficData} from "./Data/TrafficData";
+import {bindStoplichtTopicProtocol} from "./Checkers/stoplichten";
 
 async function run() {
+    console.log("Verkeer data inladen...");
+    const trafficDataRes = await fetch("https://raw.github.com/jorrit200/stoplicht-communicatie-spec/main/intersectionData/lanes.json")
+    if (!trafficDataRes.ok) {
+        throw new Error("Kon de verkeers data niet laden, deze is nodig om sommige protocol eisen te beoordelen")
+    }
+    const trafficData = await trafficDataRes.json() as TrafficData
+    console.log("data ingeladen")
+
     const sub = new Subscriber();
     sub.connect("tcp://127.0.0.1:5556");
 
-    const binder = new ZMQSubCheckerBinder(sub);
+    let binder = new ZMQSubCheckerBinder(sub);
+    binder = bindStoplichtTopicProtocol(binder)
 
     await binder.run({
         resultOutput: LogConclusionAsMarkDown
