d18de39b76868de2deffb51a565ede118dd99ff1
jorrit200 <49289098+jorrit200@users.noreply.github.com>
2025-03-22 22:07:57 +0100

    FORMAT: Css indenting in md logger is back

--- src/Loggers/LogConclusionAsMarkDown.ts
+++ src/Loggers/LogConclusionAsMarkDown.ts
@@ -16,23 +16,23 @@ export const LogConclusionAsMarkDown = async (conclusion: TopicMessageConclusion
     const style =
         "<style>" +
         ".admonition {" +
-        "border-left: 6px solid #333;" +
-        "background-color: #888;" +
-        "padding: 10px 15px;" +
-        "margin: 15px 0;" +
-        "border-radius: 5px;" +
-        "opacity: 0.8" +
+            "border-left: 6px solid #333;" +
+            "background-color: #888;" +
+            "padding: 10px 15px;" +
+            "margin: 15px 0;" +
+            "border-radius: 5px;" +
+            "opacity: 0.8" +
         "}" +
         ".admonition.info {" +
-        "border-color: #1166ee;" +
-        "background-color: #333388;" +
+            "border-color: #1166ee;" +
+            "background-color: #333388;" +
         "}" +
         ".admonition.error {" +
-        "border-color: #f44343;" +
-        "background-color: #833;" +
+            "border-color: #f44343;" +
+            "background-color: #833;" +
         "}" +
-        ".admonition:hover {" +
-        "opacity: 1" +
+            ".admonition:hover {" +
+            "opacity: 1" +
         "}" +
         "</style>"
     ;
@@ -64,9 +64,9 @@ export const LogConclusionAsMarkDown = async (conclusion: TopicMessageConclusion
 
         if (res.checker.checksFor == 'protocol' && !res.result.isOk) {
             await fs.appendFile(output, toAdmonition("error", "<strong>Protocol breuk:</strong> dat deze test niet passed betekent dat " +
-                    "dit bericht niet voldoet aan het protocol. " +
-                    "Raadpleeg <a href='https://github.com/jorrit200/stoplicht-communicatie-spec/tree/main/topics/" + conclusion.topic + "'>de specs</a> " +
-                    "en lees de errors hieronder, om aan deze eisen te voldoen.")
+                "dit bericht niet voldoet aan het protocol. " +
+                "Raadpleeg <a href='https://github.com/jorrit200/stoplicht-communicatie-spec/tree/main/topics/" + conclusion.topic + "'>de specs</a> " +
+                "en lees de errors hieronder, om aan deze eisen te voldoen.")
             )
         }
 
