da73d07de9de987de5f3e3dc561e82a9269d828c
jorrit <jorritdehaan200@gmail.com>
2025-03-26 16:22:05 +0100

    file writing is now the goat

--- src/Loggers/LogConclusionAsMarkDown.ts
+++ src/Loggers/LogConclusionAsMarkDown.ts
@@ -1,88 +1,90 @@
 ﻿import {TopicMessageConclusion} from "../Service/ZMQSubCheckerBinder";
-import {promises as fs} from 'fs';
-import * as path from 'path';
+import {promises as fsp} from 'fs'
+import {createWriteStream} from 'fs'
+import {randomUUID} from 'crypto'
+import * as path from 'path'
 
 /**
  * Takes message conclusions, and writes MarkDownFiles
  * @param conclusion Passed by binder.
  */
 export const LogConclusionAsMarkDown = async (conclusion: TopicMessageConclusion): Promise<void> => {
-    const output = `./output/${conclusion.topic}/${conclusion.timestamp}.md`
+    const output = `./output/${conclusion.topic}/${conclusion.timestamp}_${randomUUID().substring(0, 5)}.md`
     const outputDir = path.dirname(output);
 
-    await fs.mkdir(outputDir, {recursive: true});
+    await fsp.mkdir(outputDir, {recursive: true});
 
 
     const style =
         "<style>" +
         ".admonition {" +
-            "border-left: 6px solid #333;" +
-            "background-color: #888;" +
-            "padding: 10px 15px;" +
-            "margin: 15px 0;" +
-            "border-radius: 5px;" +
-            "opacity: 0.8" +
+        "border-left: 6px solid #333;" +
+        "background-color: #888;" +
+        "padding: 10px 15px;" +
+        "margin: 15px 0;" +
+        "border-radius: 5px;" +
+        "opacity: 0.8" +
         "}" +
         ".admonition.info {" +
-            "border-color: #1166ee;" +
-            "background-color: #333388;" +
+        "border-color: #1166ee;" +
+        "background-color: #333388;" +
         "}" +
         ".admonition.error {" +
-            "border-color: #f44343;" +
-            "background-color: #833;" +
+        "border-color: #f44343;" +
+        "background-color: #833;" +
         "}" +
-            ".admonition:hover {" +
-            "opacity: 1" +
+        ".admonition:hover {" +
+        "opacity: 1" +
         "}" +
         "</style>"
     ;
 
-    await fs.writeFile(output, style + '\n');
+    const stream = createWriteStream(output, {flags: 'a'});
 
-    await fs.appendFile(output, '# ' + conclusion.topic + '\n');
+    stream.write(style + '\n');
+
+    stream.write('# ' + conclusion.topic + '\n');
     const conclusionDate = new Date(conclusion.timestamp);
-    await fs.appendFile(output,
-        conclusionDate.toDateString() +
+    stream.write(conclusionDate.toDateString() +
         ' | ' +
         conclusionDate.toTimeString() +
         '\n\n'
     )
 
-    await fs.appendFile(output, "Message:\n")
+    stream.write("Message:\n")
     const messageJsonEmbed = toJsonBlock(conclusion.message);
-    await fs.appendFile(output, messageJsonEmbed);
+    stream.write(messageJsonEmbed);
 
-    await fs.appendFile(output, toAdmonition("info", "Dit bericht is parsed/deserialized en daarna weer serialized, " +
+    stream.write(toAdmonition("info", "Dit bericht is parsed/deserialized en daarna weer serialized, " +
         "bericht structuur hoeft dus nier overeen te komen met het origineel."))
 
-    await fs.appendFile(output, "\n\n");
+    stream.write("\n\n");
     for (const res of conclusion.results) {
-        await fs.appendFile(
-            output, '## ' + res.checker.name +
-            ' ' + (res.result.isOk ? '✅' : '❌') + '\n\n'
+        stream.write('## ' + res.checker.name +
+            ' ' + (res.result.isOk ? '✅' : '❌') +
+            '\n\n'
         );
 
         if (res.checker.checksFor == 'protocol' && !res.result.isOk) {
-            await fs.appendFile(output, toAdmonition("error", "<strong>Protocol breuk:</strong> dat deze test niet passed betekent dat " +
+            stream.write(toAdmonition("error", "<strong>Protocol breuk:</strong> dat deze test niet passed betekent dat " +
                 "dit bericht niet voldoet aan het protocol. " +
                 "Raadpleeg <a href='https://github.com/jorrit200/stoplicht-communicatie-spec/tree/main/topics/" + conclusion.topic + "'>de specs</a> " +
                 "en lees de errors hieronder, om aan deze eisen te voldoen.")
             )
         }
 
-        await fs.appendFile(output, toMdParagraph(res.checker.description))
+        stream.write(toMdParagraph(res.checker.description))
 
         if (!res.result.isOk) {
-            await fs.appendFile(output,
-                toUl(res.result.feedback
-                    .map(f => toAdmonition('error', f))
-                )
-            );
+            stream.write(toUl(res.result.feedback
+                .map(f => toAdmonition('error', f))));
         }
-        await fs.appendFile(output, '\n\n')
+        stream.write('\n\n')
 
     }
+    stream.end()
 }
+
 
 const toJsonBlock = (obj: {}): string => {
     return toCodeBlock('json', JSON.stringify(obj, null, 2))
